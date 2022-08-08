3daaba5636bf7e26a50d7e5af69ec9b20521b8d3
RheatheCoder <87926743+RheatheCoder@users.noreply.github.com>
2022-08-08 18:33:18 +1000

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAjRhrlIsytiL7p8je8CsItKWAi3B30qGY",
+    authDomain: "lets-chat-app-7c6a0.firebaseapp.com",
+    databaseURL: "https://lets-chat-app-7c6a0-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-app-7c6a0",
+    storageBucket: "lets-chat-app-7c6a0.appspot.com",
+    messagingSenderId: "533839468990",
+    appId: "1:533839468990:web:7fbb3b5eeac0ce4c6241ef"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+
 function addUser(){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user name", user_name);
